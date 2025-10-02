4da19841bb26d2be75190c917c8211b3bcb34f38
K. T. L. Lo <leolo@leolo.org>
2025-10-02 19:54:03 +0100

    Project File

---
+++ src/main/java/org/leolo/nrinfo/Nrinfo2025Application.java
@@ -0,0 +1,13 @@
+package org.leolo.nrinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nrinfo2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nrinfo2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/leolo/nrinfo/Nrinfo2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leolo.nrinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nrinfo2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
