21c236e173524837871bafe0cab6dd558be68866
K. T. L. Lo <leolo@leolo.org>
2026-08-01 20:10:32 +0100

    Addressing review comment

--- src/main/java/org/leolo/nrinfo/model/ai/CompletionResult.java
+++ src/main/java/org/leolo/nrinfo/model/ai/CompletionResult.java
@@ -1,34 +1,29 @@
 package org.leolo.nrinfo.model.ai;
 
 import lombok.Getter;
-import lombok.Setter;
 
-import java.util.Collections;
+import java.time.Instant;
 import java.util.List;
-import java.util.Optional;
 
 @Getter
-@Setter
 public class CompletionResult {
-    private List<String> result;
-    private long timeTaken;
-    private int totalTokens;
-    private int inputTokens;
-    private int outputTokens;
+    private final List<String> choices;
+    private final long timeTaken;
+    private final int totalTokens;
+    private final int inputTokens;
+    private final int outputTokens;
+    private final Instant createdTime;
 
     public CompletionResult(List<String> result, long timeTaken, int totalTokens, int inputTokens, int outputTokens) {
-        this.result = result;
+        this.choices = List.copyOf(result);
         this.timeTaken = timeTaken;
         this.totalTokens = totalTokens;
         this.inputTokens = inputTokens;
         this.outputTokens = outputTokens;
+        createdTime = Instant.now();
     }
 
     public CompletionResult(String result, long timeTaken, int totalTokens, int inputTokens, int outputTokens) {
-        this.result = Collections.singletonList(result);
-        this.timeTaken = timeTaken;
-        this.totalTokens = totalTokens;
-        this.inputTokens = inputTokens;
-        this.outputTokens = outputTokens;
+        this(List.of(result), timeTaken, totalTokens, inputTokens, outputTokens);
     }
 }
